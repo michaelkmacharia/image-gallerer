4a541b6eccb85e42e3e2598207b1dd25979e2e33
Michael K Macharia <104422971+universal-projects@users.noreply.github.com>
2022-09-01 09:48:27 +0300

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,41 @@
+# image-gallerer
+
+A ReactJS-based webapp for gallering users' images.
+
+## Usage
+
+```
+npm install
+
+npm run build
+
+npm run build:css
+
+npm run eject
+
+npm run start
+
+npm run test
+
+npm run watch
+
+npm run watch:css
+```
+
+## Commands
+
+```
+No commands available
+```
+
+### Versions
+
+- 1.0.0
+
+### Authors
+
+- Michael K Macharia
+
+### Licenses
+
+- MIT
---
+++ src/App.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+`use strict` ;
+
+const colors = require ( `colors` ) ;
+
+const dotenv = require ( `dotenv` ) . config () ;
+
+import React , { useState , useEffect } from `react` ;
+
+import ImageCard from `./components/ImageCard` ;
+
+import ImageSearch from `./components/ImageSearch` ;
+
+function App ()
+{
+	const [ images , setImages ] = useState ( [] ) ;
+	const [ isLoading , setIsLoading ] = useState ( true ) ;
+	const [ term , setTerm ] = useState ( `` ) ;
+	useEffect ( () =>
+		{
+			fetch ( `https://pixabay.com/api/?key=${ process . env . REACT_APP_PIXABAY_API_KEY }&q=${ term }&image_type=photo&pretty=true` ) . then ( ( response ) =>
+				{
+					response . json () ;
+					return ;
+				}
+			) .then ( ( data ) =>
+				{
+					setImages ( data . hits ) ;
+					setIsLoading ( false ) ;
+					return ;
+				}
+			) . catch ( ( error ) =>
+				{
+					console . log ( error . brightRed ) ;
+					return ;
+				}
+			) ;
+			return ;
+		} ,
+		[ term ]
+	) ;
+	return ( <div className="container mx-auto"><ImageSearch searchText={ ( text ) => setTerm ( text ) } />{ ! isLoading && images . length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1> }{ isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">{ images . map ( image => ( <ImageCard key={ image . id } image={ image } /> ) ) }</div> }</div> ) ;
+}
+
+export default App ;
